5cc7555921acb0d4b72bb48f90c3e1257e4a81dc
youki <zyx7153841@126.com>
2020-01-11 17:42:09 +0800

    用户只能看到自己发布的project的列表页与详情页

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,10 +11,23 @@ class ProjectsTest extends TestCase
     use RefreshDatabase, WithFaker;
 
     /** @test */
-    public function only_authenticated_users_can_create_project()
+    public function guests_cannot_create_project()
     {
         $attributes = factory('App\Project')->raw();
-        $this->post('projects', $attributes)->assertRedirect('/login');
+        $this->post('/projects', $attributes)->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_project()
+    {
+        $this->get('/projects')->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_a_single_project()
+    {
+        $project = factory('App\Project')->create();
+        $this->get($project->path())->assertRedirect('/login');
     }
 
     /** @test */
@@ -33,13 +46,23 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
+        $this->be(factory('App\User')->create());
         $this->withoutExceptionHandling();
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+        // $this->withoutExceptionHandling();
+        $project = factory('App\Project')->create();
+        $this->get($project->path())->assertStatus(403);
     }
 
     /** @test */
