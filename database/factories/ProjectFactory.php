dba1c5662da8d53fc25410685efcd124fe9bd282
youki <zyx7153841@126.com>
2020-01-12 11:53:28 +0800

    给列表页添加样式

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
     return [
-        'title' => $faker->sentence,
+        'title' => $faker->sentence(3),
         'description' => $faker->paragraph,
         'owner_id' => function () {
             return factory('App\User')->create()->id;
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
-</div>
-<ul>
+<header class="flex items-center mb-3 py-3">
+    <div class="w-full flex justify-between items-center">
+        <h3 class="text-grey tracking-wide">My Project</h3>
+        <a href="/projects/create" class="btn-blue">New Project</a>
+    </div>
+</header>
+
+<main class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-    <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+    <div class="lg:w-1/3 px-3 pb-6">
+        <div class="bg-white p-5 rounded-lg shadow" style="height:200px;">
+            <h3 class="font-font-normal text-xl mb-4 py-4 -ml-5 border-l-4 border-blue-light pl-4">
+                <a href="{{ $project->path() }}">{{ $project->title }}</a>
+            </h3>
+
+            <div class="text-grey">{{ \Str::limit($project->description, 100) }}</div>
+        </div>
+
+    </div>
     @empty
-    <li>No problems yet</li>
+    <div>No Problem yet</div>
     @endforelse
-</ul>
+</main>
+
 @endsection
