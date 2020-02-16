766a12e65f44fadb1c462e49f777df37a40e9ecb
youki <zyx7153841@126.com>
2020-02-17 03:41:51 +0800

    可以邀请新用户加入project（待完善）

---
+++ tests/Feature/InvitationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InvitationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_project_can_invite_a_user()
+    {
+        $project = ProjectFactory::create();
+
+        $project->invite($newUser = factory(User::class)->create());
+
+        $this->signIn($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'foo task']);
+
+        $this->assertDatabaseHas('tasks', $task);
+    }
+}
--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -31,5 +32,15 @@ class ProjectTest extends TestCase
         $task = $project->addTask('Test task');
         $this->assertCount(1, $project->tasks);
         $this->assertTrue($project->tasks->contains($task));
+    }
+
+    /** @test */
+    public function it_can_invite_a_user()
+    {
+        $project = factory('App\Project')->create();
+
+        $project->invite($user = factory(User::class)->create());
+
+        $this->assertTrue($project->members->contains($user));
     }
 }
